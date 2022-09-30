8719531e0ff4ff8498352fe6f076131457d6fafe
Pierre Banwarth <pi.banwarth@gmail.com>
2022-09-30 18:12:27 +0200

    Update main functionnality is working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,33 @@ import React from 'react';
 import Select from 'react-select';
 
 const options = [
-  {value: 0, label: 'C'},
-  {value: 1, label: 'C#'},
-  {value: 2, label: 'D'},
-  {value: 3, label: 'D#'},
-  {value: 4, label: 'E'},
-  {value: 5, label: 'F'},
-  {value: 6, label: 'F#'},
-  {value: 7, label: 'G'},
-  {value: 8, label: 'G#'},
-  {value: 9, label: 'A'},
-  {value: 10, label: 'A#'},
-  {value: 11, label: 'B'},
+  {value: 0, label: 'C',  color: '##eff8fc'},
+  {value: 1, label: 'C#', color: '#ceebf5'},
+  {value: 2, label: 'D', color: '#addeef'},
+  {value: 3, label: 'D#', color: '#8cd0e9'},
+  {value: 4, label: 'E', color: '#6bc3e2'},
+  {value: 5, label: 'F', color: '#4ab6dc'},
+  {value: 6, label: 'F#', color: '#2aa9d5'},
+  {value: 7, label: 'G', color: '#238fb5'},
+  {value: 8, label: 'G#', color: '#4#1d7594'},
+  {value: 9, label: 'A', color: '#165b73'},
+  {value: 10, label: 'A#', color: '#104152'},
+  {value: 11, label: 'B', color: '#0a2731'},
 ]
+const styles = {
+  option: (provided, state) => ({
+    ...provided,
+    fontWeight: state.isSelected ? "bold" : "normal",
+    color: state.data.color,
+    backgroundColor: state.data.bgcolor,
+    fontSize: state.selectProps.myFontSize
+  }),
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: state.data.color
+  })
+};
+
 const optionOctave = [
   { value: 0, label: '1' },
   { value: 1, label: '2' },
@@ -23,106 +37,43 @@ const optionOctave = [
   { value: 3, label: '4' },
 ]
 
-const instrumentKeyboardData = [
-  {
-    value: 0, 
-    label: 'Concertina',
-    keyboard : [
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-      [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
-    ],
-  },
-  {
-    value: 1,
-    label: 'Chroma',
-    keyboard: [
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-      [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
-    ],
-  },
-  {
-    value: 2,
-    label: 'Diato',
-    keyboard: [
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-      [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
-    ],
-  },
-  
-];
-class Touche extends React.Component {
+class Key extends React.Component {
   constructor(props) {
     super(props);
    
     this.state = {
-      push: props.dataTouche.push,
-      pull: props.dataTouche.pull,
-      octpull: props.dataTouche.octpull,
-      octpush: props.dataTouche.octpush
+      ligne: props.ligne,
+      colonne: props.colonne,
+      handleChange: props.onChange,
+      dataKey : props.dataKey,
     };
   }
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      push: nextProps.dataTouche.push,
-      pull: nextProps.dataTouche.pull,
-      octpull: nextProps.dataTouche.octpull,
-      octpush: nextProps.dataTouche.octpush
-    });
+
+  handleChangeNotePull(event) {
+     this.state.handleChange('note','pull', event, this.state)
   }
-  handleChangePush(result) {
-    this.setState({ push: result['value'] });
+  handleChangeNotePush(event) {
+    this.state.handleChange('note', 'push', event, this.state)
   }
-  handleChangePull(result) {
-    this.setState({ pull: result['value'] });
+  handleChangeOctavePull(event) {
+    this.state.handleChange('octave', 'pull', event, this.state)
   }
-  handleChangePullOctave(result) {
-    this.setState({ octpull: result['value'] });
+  handleChangeOctavePush(event) {
+    this.state.handleChange('octave', 'push', event, this.state)
   }
-  handleChangePushOctave(result) {
-    this.setState({ octpush: result['value'] });
-  }
+
   render() {
     return <td>
       <table>
         <tbody>
           <tr>
             <td>
-              <Select options={options} value={options[this.state.pull]} onChange={this.handleChangePull.bind(this)} />
-              <Select options={options} value={options[this.state.push]} onChange={this.handleChangePush.bind(this)} />
+              <Select className='note-select'  styles={styles} options={options} value={options[this.props.dataKey.pull]} onChange={this.handleChangeNotePull.bind(this)} />
+              <Select className='note-select'  styles={styles} options={options} value={options[this.props.dataKey.push]} onChange={this.handleChangeNotePush.bind(this)} />
             </td>
             <td>
-              <Select options={optionOctave} value={optionOctave[this.state.octpull]} onChange={this.handleChangePullOctave.bind(this)} />
-              <Select options={optionOctave} value={optionOctave[this.state.octpush]} onChange={this.handleChangePushOctave.bind(this)} />
+              <Select options={optionOctave} value={optionOctave[this.props.dataKey.octpull]} onChange={this.handleChangeOctavePull.bind(this)} />
+              <Select options={optionOctave} value={optionOctave[this.props.dataKey.octpush]} onChange={this.handleChangeOctavePush.bind(this)} />
             </td>
           </tr>
 
@@ -137,50 +88,160 @@ class Touche extends React.Component {
 class Keyboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { selectedKeyboard: props.selectedKeyboard}
+    this.state = {
+      handleChangeKey: props.onChange,
+    }
   }
-  componentWillReceiveProps(nextProps) {
-    this.setState({ selectedKeyboard: nextProps.selectedKeyboard });
+  handleChange = (type, pushpull, event, buttonState) => {
+    this.state.handleChangeKey(type, pushpull ,event, buttonState)
   }
-  render() {
+  render() {    
     return (    
-      this.state.selectedKeyboard.keyboard.map((keyboard, index) =>
-        <tr key={index*2}>
-          <Touche dataTouche={keyboard[0]} ></Touche>
-          <Touche dataTouche={keyboard[1]} ></Touche>
-          <Touche dataTouche={keyboard[2]} ></Touche>
+      this.props.keyboard.map((row, index) =>
+        <tr key={index}>
+          {
+            row.map(
+              (y, ind) =>  <Key key={index+1*ind+1} dataKey={y} onChange={this.handleChange} ligne={index} colonne={ind}></Key>
+            )
+          }
         </tr>
       )
     )
   }
 }
+const optionKeyboardDefault = [
+  {
+    value: 0, 
+    label: 'Anglo Concertina'
+  },{
+    value: 1,
+    label: 'Accordéon Chromatique',
+  },{
+    value: 2,
+    label: 'Accordéon Diatonique (Sol/Do)',
+  }
+]
 
+const instrumentKeyboardData = [
+  [
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+    [{ push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}, { push: 4, pull: 4 , octpull : 1, octpush : 1}],
+  ],
+  [
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+    [{ push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}, { push: 5, pull: 5 , octpull : 1, octpush : 1}],
+  ],[
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+    [{ push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}, { push: 6, pull: 6 , octpull : 1, octpush : 1}],
+  ],
+]
 class KeyboardManager extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      selectedKeyboard: instrumentKeyboardData[0],
-    };
+      selectedKeyboard: 0,
+      currentKeyboard: instrumentKeyboardData[0]
+    }
   }
 
-  handleChangeKeyboard(result) {
-    this.setState({ selectedKeyboard: result });
+  handleChangeKeyboard(e) {
+    var tmp = JSON.parse(JSON.stringify(instrumentKeyboardData[e.value]));
+    this.setState({
+      selectedKeyboard: e.value,
+      currentKeyboard: tmp
+    })
+  }
+
+  handleChangeKey(type, pushpull, e, buttonState) {
+    var tempKeyboard = this.state.currentKeyboard
+    if(type === 'note'){
+      tempKeyboard[buttonState.ligne][buttonState.colonne][pushpull] = e.value
+      this.setState({
+        currentKeyboard : tempKeyboard
+      })
+    }else if(type === 'octave'){
+      if(pushpull === 'push'){
+        tempKeyboard[buttonState.ligne][buttonState.colonne]['octpush'] = e.value
+        this.setState({
+          currentKeyboard : tempKeyboard
+        })
+      }else if(pushpull ==='pull'){
+        tempKeyboard[buttonState.ligne][buttonState.colonne]['octpull'] = e.value
+        this.setState({
+          currentKeyboard : tempKeyboard
+        })
+      }
+    }
+  }
+
+  FlattenArray(){
+    var result = [
+      [],[]
+    ]
+    var test = [].concat.apply([], this.state.currentKeyboard)
+    var push = test.map((
+      (row) => row.push + 12*row.octpush)
+    )
+    result[0] = push
+    var pull = test.map((
+      (row) => row.pull + 12*row.octpull)
+    )
+    result[1] = pull
+    
+    return result;
   }
 
   render() {
+    let value = this.FlattenArray().map(e=>JSON.stringify(e)).join(',\n');
+
     return (
-      <div>
+      <div className='main-content'>
         <Select
-          options={instrumentKeyboardData}
-          defaultValue={this.state.selectedKeyboard}
+          options={optionKeyboardDefault}
+          value={optionKeyboardDefault[this.state.selectedKeyboard]}
           onChange={this.handleChangeKeyboard.bind(this)}
         />
-        <table>
-          <tbody>
-            <Keyboard selectedKeyboard={this.state.selectedKeyboard}/>
-          </tbody>
-        </table>
+        <div className='array-area'>
+          <table>
+            <tbody>
+              <Keyboard keyboard={this.state.currentKeyboard} onChange={this.handleChangeKey.bind(this)}/>
+            </tbody>
+          </table>
+          <div className='result-area'>
+            <label>Result</label>
+            <textarea readOnly value={value}/>
+          </div>
+        </div>
       </div>
     );
   }
